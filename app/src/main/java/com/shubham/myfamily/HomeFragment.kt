8d864dab9c361f4aea8f68b82b8241c9a8d1912e
Shubham Kumar <115333492+shubhamkumaar@users.noreply.github.com>
2024-02-12 00:35:29 +0530

    recyler view and Maps

--- app/src/main/java/com/shubham/myfamily/HomeFragment.kt
+++ app/src/main/java/com/shubham/myfamily/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,21 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onViewCreated(view:View,savedInstanceState: Bundle?){
+        super.onViewCreated(view,savedInstanceState)
+
+        val listMember = listOf<MemberModel>(
+            MemberModel("Madhur"),
+            MemberModel("Shubham"),
+            MemberModel("Pranjal"),
+            MemberModel("Chutiya"),
+        )
+
+        val adapter = MemberAdapter(listMember)
+        val recycler = requireView().findViewById<RecyclerView>(R.id.recycler_member)
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
     }
 
     companion object {
---
+++ app/src/main/java/com/shubham/myfamily/MemberModel.kt
@@ -0,0 +1,5 @@
+package com.shubham.myfamily
+
+data class MemberModel(
+    val name:String
+)
